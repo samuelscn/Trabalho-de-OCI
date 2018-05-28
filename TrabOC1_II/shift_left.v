af656f8735780b7740c0f382b17cc2e2ad885e90
zamuca303 <31083326+zamuca303@users.noreply.github.com>
2018-05-28 10:43:13 -0300

    Delete shift_left.v

--- TrabOC1_II/shift_left.v
+++
@@ -1,8 +0,0 @@
-module shift_left(endereco_32bits, endereco_deslocado);
-  input [31:0] endereco_32bits;
-  output reg [31:0] endereco_deslocado;
-
-  always@(endereco_32bits) begin
-    endereco_deslocado = endereco_32bits << 2;
-  end
-endmodule
